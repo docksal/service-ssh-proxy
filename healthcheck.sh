947b14b9a8670ef29d96d1460c7daf8b4911570b
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2019-12-19 10:40:44 +0200

    Healthcheck update. switch to ps check method

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# supervisor services are running
-if [[ -f /run/supervisord.pid ]]; then
-	supervisorctl status sshpiperd | grep RUNNING >/dev/null || exit 1
-	supervisorctl status crond | grep RUNNING >/dev/null || exit 1
+# check sshd process is running
+ps | grep -v 'grep' | grep sshpiperd >/dev/null 2>&1 || exit 1
 
-	exit 0
-fi
+# check crond process is running
+ps | grep -v 'grep' | grep crond >/dev/null 2>&1 || exit 1
 
-exit 1
+exit 0
